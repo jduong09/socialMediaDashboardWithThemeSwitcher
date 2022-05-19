26327ed9ae8aa2dcb877d8f31001bbd34a9f95f7
Justin Duong <jduong@umich.edu>
2022-05-19 14:54:06 -0400

    Used css grid to create additional columns and rows for tablet and desktop view.

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btnToggle = document.getElementById("btn-toggle");
+  const bodyElement = document.getElementsByTagName('body')[0];
   const mainElement = document.getElementsByTagName('main')[0];
   btnToggle.addEventListener('click', () => {
     if (mainElement.classList.contains('dark')) {
+      bodyElement.classList.remove('dark');
       mainElement.classList.remove('dark');
       btnToggle.classList.remove('dark');
     } else {
+      bodyElement.classList.add('dark');
       mainElement.classList.add('dark');
       btnToggle.classList.add('dark');
     }
